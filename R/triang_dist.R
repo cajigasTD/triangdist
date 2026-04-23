b13735396ebb5faa494dbf6c636843470888751a
cajigasTD <cajigas@alumni.uv.es>
2026-04-23 21:15:51 +0200

    dtriang

--- R/triang_dist.R
+++ R/triang_dist.R
@@ -7,3 +7,26 @@ check_tri_params <- function(min, max, mode) {
     stop("`mode` must be between `min` and `max`.")
   }
 }
+
+
+dtriang <- function(x, min, max, mode) {
+
+  check_tri_params(min, max, mode)
+
+  f <- numeric(length(x))
+
+  for (i in 1:length(x)) {
+
+    if (x[i] < min || x[i] > max) {
+      f[i] <- 0
+
+    } else if (x[i] <= mode) {
+      f[i] <- 2 * (x[i] - min) / ((max - min) * (mode - min))
+
+    } else {
+      f[i] <- 2 * (max - x[i]) / ((max - min) * (max - mode))
+    }
+  }
+
+  return(f)
+}
